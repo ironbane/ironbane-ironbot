b89fd199a263d8c13d460c615eacd66b8137b3c4
Lars van der Schans <larsvanderschans@gmail.com>
2014-11-10 00:26:47 +0100

    a

--- update_dev.sh
+++ update_dev.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+cd /opt/ironbane-dev-server/
+git pull
+git checkout master
+git reset --hard
+
+
+#ironbane-dev-server
+#ironbane-ironbot
+#ironbane-play-server
+#ironbane-router
+#ironbane-secret
