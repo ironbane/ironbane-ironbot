964d1c463cba0dcc2a41d12a9bf0cc2f253337a4
Lars van der Schans <larsvanderschans@gmail.com>
2014-11-10 00:30:04 +0100

    aaa

--- update_dev.sh
+++ update_dev.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+sleep 5
 cd /opt/ironbane-dev-server/
 git pull
 git checkout master
 git reset --hard
-
+sudo stop ironbane-dev-server
+sudo start ironbane-dev-server
 
 #ironbane-dev-server
 #ironbane-ironbot
