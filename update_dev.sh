643f9828c798d7bb3547cf5eb9634ffe179bbec0
Lars van der Schans <larsvanderschans@gmail.com>
2014-11-19 09:40:57 +0100

    Some changes

--- update_dev.sh
+++ update_dev.sh
@@ -3,6 +3,7 @@ sleep 5
 cd /opt/ironbane-dev-server/
 /usr/bin/git pull
 /usr/bin/git checkout master
+/usr/bin/nodejs /opt/ironbane-ironbot/notify.js "running npm install" blue
 /usr/bin/sudo npm install
 /usr/bin/sudo restart ironbane-dev-server
 /usr/bin/nodejs /opt/ironbane-ironbot/notify.js "ironbane-dev-server up2date and restarted" yellow
--- update_ironbot.sh
+++ update_ironbot.sh
@@ -3,5 +3,7 @@ sleep 5
 cd /opt/ironbane-ironbot/
 /usr/bin/git pull
 /usr/bin/git checkout master
+/usr/bin/nodejs /opt/ironbane-ironbot/notify.js "running npm install" blue
+/usr/bin/sudo npm install
 /usr/bin/nodejs /opt/ironbane-ironbot/notify.js "I am up2date and will restart myself" blue
 /usr/bin/sudo restart ironbane-ironbot
