32d584e4d3835e5390f358ab5ba4ee3a1cdcccca
Ben Sparks <bsparks@unicon.net>
2014-12-11 16:29:43 -0700

    base play server off tags

--- update_play.sh
+++ update_play.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 sleep 5
 cd /opt/ironbane-play-server/
-/usr/bin/git pull
-/usr/bin/git checkout master
+# Get new tags from remote
+/usr/bin/sudo git fetch --tags
+# Get latest tag name
+/usr/bin/sudo latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
+# Checkout latest tag
+/usr/bin/sudo git checkout $latestTag
 /usr/bin/sudo restart ironbane-play-server
 /usr/bin/nodejs /opt/ironbane-ironbot/notify.js "ironbane-play-server up2date and restarted" red
